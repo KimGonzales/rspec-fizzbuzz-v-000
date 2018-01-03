d5fa742124d269248f76e5c5ffcc293207e0273b
Kim Gonzales <kimgo589@gmail.com>
2018-01-03 22:21:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(int)
     if int % 3 == 0
       "Fizz"
 
-  elsif int % 5 == 0 && (int % 3) !=0
+  elsif int % 5 == 0
     "Buzz"
 
-  else int % 3 == 0 && int % 5
+  else int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
 end
